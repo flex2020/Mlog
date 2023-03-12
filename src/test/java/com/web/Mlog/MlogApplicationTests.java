7d6a60a45eb2448a55841954350ac4df2ec578a9
NOH UIBIN <hosan_1@naver.com>
2023-03-12 21:39:57 +0900

    Initial commit

---
+++ src/test/java/com/web/Mlog/MlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.Mlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
