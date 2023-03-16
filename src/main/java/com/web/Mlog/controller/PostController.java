35f31f220655a5b94617f4f7ce3bf81411865ae4
NOH UIBIN <hosan_1@naver.com>
2023-03-16 23:00:53 +0900

    update: controller 스켈레톤 작성

--- src/main/java/com/web/Mlog/controller/PostController.java
+++ src/main/java/com/web/Mlog/controller/PostController.java
@@ -1,10 +1,15 @@
 package com.web.Mlog.controller;
 
+import com.web.Mlog.dto.PostDto;
 import com.web.Mlog.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/post")
 public class PostController {
     private PostService postService;
 
@@ -12,4 +17,45 @@ public class PostController {
     public PostController(PostService postService) {
         this.postService = postService;
     }
+
+    /**
+     * 포스트 목록
+     * */
+    @GetMapping("")
+    public List<PostDto.PostListDto> postList() {
+
+    }
+
+    /**
+     * 포스트 조회
+     * */
+    @GetMapping("/{postId}")
+    public PostDto.PostDetailsDto postDetails(@PathVariable long postId) {
+
+    }
+
+    /**
+     * 포스트 등록 처리
+     * */
+    @PostMapping("")
+    public PostDto.PostAddDto postAdd() {
+
+    }
+
+    /**
+     * 포스트 삭제 처리
+     * */
+    @DeleteMapping("")
+    public PostDto.PostDeleteDto postDelete() {
+
+    }
+
+    /**
+     * 포스트 수정 처리
+     * */
+    @PutMapping("")
+    public PostDto.PostModifyDto postModify() {
+
+    }
+
 }
