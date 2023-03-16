0713bf04d6b4da2adb75ac213786a8e8f174bf84
NOH UIBIN <hosan_1@naver.com>
2023-03-16 23:01:15 +0900

    update: PostDto 스켈레톤 작성

---
+++ src/main/java/com/web/Mlog/dto/PostDto.java
@@ -0,0 +1,24 @@
+package com.web.Mlog.dto;
+
+public class PostDto {
+    public static class PostListDto {
+         int postId;
+
+    }
+
+    public static class PostDetailsDto {
+
+    }
+
+    public static class PostAddDto {
+
+    }
+
+    public static class PostDeleteDto {
+
+    }
+
+    public static class PostModifyDto {
+
+    }
+}
