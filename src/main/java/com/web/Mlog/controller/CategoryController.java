5ca8011d3b82ddf67d2cc94da68e84240283f6a5
NOH UIBIN <hosan_1@naver.com>
2023-03-19 15:10:36 +0900

    feat: Category 등록

--- src/main/java/com/web/Mlog/controller/CategoryController.java
+++ src/main/java/com/web/Mlog/controller/CategoryController.java
@@ -2,15 +2,27 @@ package com.web.Mlog.controller;
 
 import com.web.Mlog.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/category")
 public class CategoryController {
     private CategoryService categoryService;
 
     @Autowired
     public CategoryController(CategoryService categoryService) {
         this.categoryService = categoryService;
+    }
+
+    /**
+     * 카테고리 추가
+     * */
+    @PostMapping("/{categoryName}")
+    public boolean addCategory(@PathVariable String categoryName) {
+        return categoryService.addCategory(categoryName);
     }
 
 }
--- src/main/java/com/web/Mlog/service/CategoryService.java
+++ src/main/java/com/web/Mlog/service/CategoryService.java
@@ -1,8 +1,11 @@
 package com.web.Mlog.service;
 
+import com.web.Mlog.domain.Category;
 import com.web.Mlog.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class CategoryService {
@@ -11,5 +14,14 @@ public class CategoryService {
     @Autowired
     public CategoryService(CategoryRepository categoryRepository) {
         this.categoryRepository = categoryRepository;
+    }
+
+    public boolean addCategory(String categoryName) {
+        // 카테고리가 이미 존재하는 경우
+        System.out.println(categoryName);
+        if(categoryRepository.existsById(categoryName)) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "이미 존재하는 카테고리입니다.");
+        }
+        return categoryName.equals(categoryRepository.save(new Category(categoryName)).getCategoryName());
     }
 }
